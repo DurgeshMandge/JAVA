284218d24298365e9d2dc6e3e83992d020239f63
Durgesh Mandge <durgeshdmd4602@gmail.com>
2023-12-27 21:19:07 +0530

    sorting file added

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,13 @@
+package Sorting;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,1,7,6,2};
+        quickSort(arr);
+        System.out.println(arr);
+    }
+
+    private static void quickSort(int[] arr) {
+        
+    }
+}
