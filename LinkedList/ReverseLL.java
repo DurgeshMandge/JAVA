e6cdcdef4903f6d30a6c6c41e34ef2992ddd4d1c
Durgesh Mandge <durgeshdmd4602@gmail.com>
2024-01-13 16:43:06 +0530

    Trees

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -1,7 +1,21 @@
 package LinkedList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseLL {
     public static void main(String[] args) {
-        
+        List<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        l.add(6);
+        l.add(7);
+        Integer[] arr = l.toArray(new int[0]);
+        List<Integer> s = l.subList(1, l.size());
+        System.out.println(s);
+        System.out.println(l.get(0));
     }
 }
---
+++ Trees/Basic.java
@@ -0,0 +1,69 @@
+package Trees;
+
+import java.util.Scanner;
+
+public class Basic {
+    
+    private class Node {
+        int val;
+        Node right;
+        Node left;
+        public Node(int value){
+            this.val = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Basic b = new Basic();
+        Node root = b.new Node(5);
+        //Create the tree
+        populateTree(root);
+
+        //Print it
+        preetyPrint(root,0);
+    }
+    
+    private static void preetyPrint(Node root,int indent) {
+        if(root == null){return;}
+        preetyPrint(root.right,indent++);
+        if(indent != 0){
+            for(int i = 0;i<indent;i++){
+                System.out.printf("|\t\t");
+            }
+            System.out.println("|----> " + root.val);
+        }else{
+            System.out.println(root.val);
+        }
+        
+        preetyPrint(root.right, indent++);
+    }
+
+    private static void populateTree(Node node) {
+        Scanner sc = new Scanner(System.in);
+        helper(node,sc);
+    }
+
+    private static void helper(Node node, Scanner sc) {
+        if(node == null){return;}
+        System.out.println("Do u want to add left of " + node.val);
+        boolean ansL = sc.nextBoolean();
+        if(ansL){
+            System.out.println("Give value : ");
+            int val = sc.nextInt();
+            Basic b = new Basic();
+            node.left = b.new Node(val);
+            helper(node.left, sc);
+        }
+        System.out.println("Do u want to add right " + node.val);
+        boolean ansR = sc.nextBoolean();
+        if(ansR){
+            System.out.println("Give value : ");
+            int val = sc.nextInt();
+            Basic b = new Basic();
+            node.right = b.new Node(val);
+            helper(node.right, sc);
+        }
+        return;
+    }
+
+}
